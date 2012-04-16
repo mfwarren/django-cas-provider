604bc4aa10c2c8d353e4c2fbc0392dcfa9543fdd
deyk <deyk@crossway.org>
2012-04-16 11:06:52 -0700

    Fixed socialauth-login url.

--- cas_provider/urls.py
+++ cas_provider/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('cas_provider.views',
     url(r'^login/merge/', 'login', {'merge': True, 'template_name': 'cas/merge.html'}),
     url(r'^login/?$', 'login', name='cas_login'),
-    url(r'^socialauth-login/$', 'socialauth_login', name='cas_socialauth_login'),
+    url(r'^socialauth-login/$', 'login', name='cas_socialauth_login'),
     url(r'^validate/?$', 'validate', name='cas_validate'),
     url(r'^proxy/?$', 'proxy', name='proxy'),
     url(r'^serviceValidate/?$', 'service_validate', name='cas_service_validate'),
