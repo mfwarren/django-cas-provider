7261dd34a1eccc4508f51a2daecb6e8cfd5bbc61
deyk <deyk@crossway.org>
2013-04-17 15:32:52 -0700

    Added new signals.

--- cas_provider/signals.py
+++ cas_provider/signals.py
@@ -9,5 +9,10 @@ on_cas_collect_histories = dispatch.Signal(providing_args=["for_email"])
 on_cas_login = dispatch.Signal(providing_args=["request"])
 
 on_cas_login_success = dispatch.Signal(providing_args=["user", "service"])
+on_cas_login_failure = dispatch.Signal(providing_args=["service"])
+
+on_cas_proxy_success = dispatch.Signal(providing_args=["service"])
+on_cas_validation_success = dispatch.Signal(providing_args=["service"])
+on_cas_validation_failure = dispatch.Signal(providing_args=["code", "message", "service"])
 
 cas_collect_custom_attributes = dispatch.Signal(providing_args=['user'])
